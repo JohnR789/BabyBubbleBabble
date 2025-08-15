a07f5a344e5eed93b66935c35dd3e679cc187f46
JohnR789 <rollinsj789@gmail.com>
2025-08-15 15:53:25 -0400

    checkpoint before switching to Expo Go

--- metro.config.js
+++ metro.config.js
@@ -1,7 +1,2 @@
-// metro.config.js — Expo default
 const { getDefaultConfig } = require('expo/metro-config');
-
-const config = getDefaultConfig(__dirname);
-
-module.exports = config;
-
+module.exports = getDefaultConfig(__dirname);
